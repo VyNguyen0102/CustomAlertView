31c3827f0819070a4bcd3baf1322be9efbfe3c6c
Vy Nguyen <nguyenvanvy0102@gmail.com>
2015-12-25 21:56:26 +0700

    imp. close function

--- CustomAlertView/CustomAlertView/NVAlertView.swift
+++ CustomAlertView/CustomAlertView/NVAlertView.swift
@@ -19,10 +19,13 @@ class NVAlertView: NSObject {
     var alertBackground:UIView?
     var closeButton:UIButton?
     
+    // Release retain
+    var retainedSelves:NSMutableArray?
     
     init(rootView:UIView, buttonTitle:NSString){
         self.rootView = rootView
         self.buttonTitle = buttonTitle
+        retainedSelves = NSMutableArray()
     }
     
     func showPopUp() {
@@ -38,8 +41,11 @@ class NVAlertView: NSObject {
         self.panelView = NSBundle.mainBundle().loadNibNamed("NVAlertView", owner: self, options: nil)[0] as? UIView
         self.panelView?.translatesAutoresizingMaskIntoConstraints = false;
         self.alertBackground = self.panelView!.viewWithTag(0)!;
+        self.closeButton = self.panelView!.viewWithTag(2) as? UIButton;
+        self.closeButton?.addTarget(self, action: Selector("closeAlertDialog:"), forControlEvents: .TouchUpInside)
         self.alertBackground!.clipsToBounds = true;
         self.layoutToRootView()
+        self.retainSelf()
     }
     
     func layoutToRootView() {
@@ -60,5 +66,16 @@ class NVAlertView: NSObject {
         self.rootView.addConstraint(trailing)
         self.rootView.addConstraint(leading)
         NSLayoutConstraint.activateConstraints([bottom, trailing, top, leading])
+    }
+    
+    func closeAlertDialog(sender: UIButton) {
+        self.panelView?.removeFromSuperview()
+        self.releaseSelf()
+    }
+    func retainSelf() {
+        self.retainedSelves!.addObject(self)
+    }
+    func releaseSelf() {
+        self.retainedSelves!.removeLastObject()
     }
 }
